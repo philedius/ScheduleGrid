06c439d2fb5dc6f31899c1bd4c3dece3d84ea4df
philedius <fannarr@gmail.com>
2018-07-04 14:48:31 +0200

    Add DayJS. Started working on timeline logic. Refactoring.

--- jquery.ScheduleGrid.js
+++ jquery.ScheduleGrid.js
@@ -1,69 +1,61 @@
 (function ($) {
-
+    
     var resourceCellWidth = 180;
-
+    var numColumns;
+    var eventWidth;
+    var eventHeight;
+    var cellW;
+    var cellH;
+    var numRows = 60;
+    var firstNames = ['Jeff', 'Alex', 'Vinny', 'Ryan', 'Abby', 'Drew', 'Brad', 'Patrick', 'Dan', 'Jason', 'Jan', 'Austin', 'Ben', 'Dave', 'Brett'];
+    var lastNames = ['Gerstmann', 'Navarro', 'Caravella', 'Davis', 'Russell', 'Scanlon', 'Shoemaker', 'Klepek', 'Ryckert', 'Oestreicher', 'Ochoa', 'Walker', 'Pack', 'Snider'];
+    
     $.fn.ScheduleGrid = function (options) {
         $grid = this;
         var settings = $.extend({
-            cellX: 32,
-            cellY: 32,
-            gridHeight: 700,
-            eventMargin: 10,
+            cell: {
+                size: {
+                    width: 36,
+                    height: 36
+                },
+            },
+            event: {
+                margin: 10
+            },
             resizable: true,
-            draggable: true
+            draggable: true,
+            numMonthsToRender: 3
         }, options);
-
-        var numDaysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+        
         var colors = {
-            'blue': 'rgb(79, 146, 221)',
-            'red': 'rgb(201, 59, 24)',
-            'yellow': 'rgb(230, 190, 0)',
-            'orange': 'rgb(240, 110, 0)',
+            'blue': 'rgb(37, 120, 220)',
+            'red': 'rgb(232, 45, 45)',
+            'yellow': 'rgb(243, 200, 0)',
+            'orange': 'rgb(239, 109, 0)',
             'green': 'rgb(25, 170, 72)'
         };
-        var numRows = 60;
-        var firstNames = ['Jeff', 'Alex', 'Vinny', 'Ryan', 'Abby', 'Drew', 'Brad', 'Patrick', 'Dan', 'Jason', 'Jan', 'Austin', 'Ben', 'Dave', 'Brett'];
-        var lastNames = ['Gerstmann', 'Navarro', 'Caravella', 'Davis', 'Russell', 'Scanlon', 'Shoemaker', 'Klepek', 'Ryckert', 'Oestreicher', 'Ochoa', 'Walker', 'Pack', 'Snider'];
         settings.numTimelineRows = $('.timeline-row').length;
         var scrollbarWidth = 17;
-        settings.eventWidth = settings.cellX - settings.eventMargin;
-        settings.eventHeight = settings.cellY - settings.eventMargin;
-        var numRows = 60;
-        settings.numColumns = numDaysInMonth[new Date().getMonth()];
+        cellW = settings.cell.size.width;
+        cellH = settings.cell.size.height;
+        eventWidth = cellW - settings.event.margin;
+        eventHeight = cellH - settings.event.margin;
+        numColumns = calculateNumColumns(settings.numMonthsToRender);
 
-        var gridHTML = '';
-        for (var i = 0; i < numRows; i++) {
-            var firstName = firstNames[Math.floor(Math.random() * firstNames.length)];
-            var lastName = lastNames[Math.floor(Math.random() * lastNames.length)];
-            gridHTML += '<div class="row" data-row="' + i + '"><div class="cell resource-cell"><div class="resource-name">' + firstName + ' ' + lastName + '</div></div>';
-            for (var j = 0; j < settings.numColumns; j++) {
-                gridHTML += '<div class="cell" data-x="' + j + '" data-y="' + i + '"></div>';
-            }
-            gridHTML += '</div>';
-        }
-        $($grid).append(gridHTML);
-
-        var daysHTML = '';
-        for (var i = 0; i < settings.numColumns; i++) {
-            var day = i + 1;
-            daysHTML += '<div class="cell workday">' + day + '</div>';
-        }
-
-        $('#days').append(daysHTML);
+        setupTimeline(settings);
 
         if (isIE()) IEScrollFix(settings);
         setDimensions(settings);
         
-        // $('#modal-form').modal();
         $('.event').each(function () {
             var startingY = $(this).data('y');
             var startingX = $(this).data('x');
             var span = $(this).data('span');
             var color = $(this).data('color');
             $(this).css({
-                'top': (startingY * settings.cellY) + 'px',
-                'left': (startingX * settings.cellX) + 'px',
-                'width': (span * settings.cellX) + 'px',
+                'top': (startingY * cellH) + 'px',
+                'left': (startingX * cellW) + 'px',
+                'width': (span * cellW) + 'px',
             });
             $(this).find('.event-text').css({
                 'background': colors[color],
@@ -86,23 +78,51 @@
             });
 
             if ($(this).scrollTop() > 0) {
-                $('.timeline-row').css('box-shadow', '0 1px 5px rgba(0, 0, 0, .06)');
+                $('.timeline-row:last').css('box-shadow', '0 1px 5px rgba(0, 0, 0, .06)');
             } else {
-                $('.timeline-row').css('box-shadow', 'none');
+                $('.timeline-row:last').css('box-shadow', 'none');
             }
         });
 
-        $($grid).css('opacity', 1);
         return this;
+    }
+
+    function setupTimeline(settings) {
+        setupTimelineHeaders(settings);
+        var gridHTML = '';
+        for (var i = 0; i < numRows; i++) {
+            var firstName = firstNames[Math.floor(Math.random() * firstNames.length)];
+            var lastName = lastNames[Math.floor(Math.random() * lastNames.length)];
+            gridHTML += '<div class="row" data-row="' + i + '"><div class="cell resource-cell"><div class="resource-name">' + firstName + ' ' + lastName + '</div></div>';
+            for (var j = 0; j < numColumns; j++) {
+                gridHTML += '<div class="cell" data-x="' + j + '" data-y="' + i + '"></div>';
+            }
+            gridHTML += '</div>';
+        }
+        $($grid).append(gridHTML);
+    }
+
+    function setupTimelineHeaders(settings) {
+        var daysHTML = '';
+        for (var i = 0; i < settings.numMonthsToRender; i++) {
+            var days = dayjs().add(i, 'month').daysInMonth();
+            var monthWidth = days * cellW;
+            $('#months').append('<div style="min-width: ' + monthWidth + 'px !important; width: ' + monthWidth + 'px" class="cell">' + dayjs().add(i, 'month').format('MMMM') + '</div>')
+            for (var j = 0; j < days; j++) {
+                var day = j + 1;
+                daysHTML += '<div class="cell">' + day + '</div>';
+            }
+        }
+        $('#days').append(daysHTML);
     }
 
     function makeResizable(settings, element) {
         if (!element) element = $('.event');
         element.resizable({
-            grid: [settings.cellX, settings.cellY],
+            grid: [cellW, cellH],
             handles: 'e',
             stop: function (event, ui) {
-                var spaces = (ui.size.width - ui.originalSize.width) / settings.cellX;
+                var spaces = (ui.size.width - ui.originalSize.width) / cellW;
                 ui.element.attr('data-span', parseInt(ui.element.attr('data-span')) + spaces);
                 var x = parseInt($(ui.element).attr('data-x'));
                 var y = parseInt($(ui.element).attr('data-y'));
@@ -115,21 +135,20 @@
     function makeDraggable(settings, element) {
         if (!element) element = $('.event');
         var x1 = $grid.offset().left + resourceCellWidth;
-        var y1 = $grid.offset().top + (settings.cellX / 2);
-        var x2 = $grid.offset().left + ((settings.numColumns * settings.cellX) + resourceCellWidth) - element.width();
-        var y2 = $grid.offset().top + ($('.row').length * settings.cellY);
+        var y1 = $grid.offset().top + (cellH * settings.numTimelineRows);
+        var x2 = $grid.offset().left + ((numColumns * cellW) + resourceCellWidth) - element.width();
+        var y2 = $grid.offset().top + ($('.row').length * cellH);
         var boundingBox = [x1, y1, x2, y2];
         element.draggable({
             stacks: '.event',
-            grid: [settings.cellX, settings.cellY],
+            grid: [cellW, cellH],
             containment: boundingBox,
             stop: function (event, ui) {
-                console.log(boundingBox);
-                var spaces = $(event.target).width() / settings.cellX;
+                var spaces = $(event.target).width() / cellW;
                 var posTop = parseInt($(event.target).css('top').replace('px', ''));
                 var posLeft = parseInt($(event.target).css('left').replace('px', ''));
-                var newY = posTop / settings.cellY;
-                var newX = posLeft / settings.cellX;
+                var newY = posTop / cellH;
+                var newX = posLeft / cellW;
                 $(event.target).attr('data-y', newY);
                 $(event.target).attr('data-x', newX);
                 $(event.target).find('.event-text').text($('.row[data-row="' + newY + '"] .resource-cell').text() + ' ' + (newX + 1) + '. - ' + (newX + 1 + parseInt($(event.target).attr('data-span')) - 1) + '. Juli');
@@ -140,37 +159,35 @@
 
     function setDimensions(settings) {
         $('.cell').css({
-            'min-width': settings.cellX,
-            'height': settings.cellY,
-            'min-height': settings.cellY,
-            'max-height': settings.cellY,
+            'min-width': cellW,
+            'height': cellH,
+            'min-height': cellH,
+            'max-height': cellH,
         });
     
-        $('#months .cell').css('width', settings.cellX * settings.numColumns);
-    
         $('.event').css({
-            'min-width': settings.cellX,
-            'height': settings.cellY,
-            'min-height': settings.cellY,
-            'max-height': settings.cellY,
+            'min-width': cellW,
+            'height': cellH,
+            'min-height': cellH,
+            'max-height': cellH,
         });
     
         $('.event-text').css({
-            'margin-left': settings.eventMargin / 2,
-            'margin-right': settings.eventMargin / 2,
-            'min-width': settings.eventWidth,
-            'height': settings.eventHeight,
-            'min-height': settings.eventHeight,
-            'max-height': settings.eventHeight,
+            'margin-left': settings.event.margin / 2,
+            'margin-right': settings.event.margin / 2,
+            'min-width': eventWidth - (settings.event.margin / 2),
+            'height': eventHeight,
+            'min-height': eventHeight,
+            'max-height': eventHeight,
     
         });
     
         $('.resource-cell').css('width', resourceCellWidth);
-        $('.row').css('height', settings.cellY);
-        $grid.css('height', settings.gridHeight);
-        
+        $('.resource-cell').css('min-width', resourceCellWidth);
+        $('.row').css('height', cellH);
+        $('.row').css('width', (cellW * numColumns) + resourceCellWidth);
         $('.events').css('width', $('.events').width() - resourceCellWidth);
-        $('.events').css('top', settings.numTimelineRows * settings.cellY);
+        $('.events').css('top', (settings.numTimelineRows * cellH));
         $('.events').css('left', $('.resource-cell').width());
     }
 
@@ -185,6 +202,14 @@
             var currentScrollPosition = $($grid).scrollTop();
             $($grid).scrollTop(currentScrollPosition - wheelDelta);
         });
+    }
+
+    function calculateNumColumns(numMonths) {
+        var numDays = 0;
+        for (var i = 0; i < numMonths; i++) {
+            numDays += dayjs().add(i, 'month').daysInMonth();
+        }
+        return numDays;
     }
 
 })(jQuery);
